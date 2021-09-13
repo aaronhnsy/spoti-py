6911f6a0760832878e5e88a5ab3df6d5a0063cf8
Aaron Hennessey <axelancerr@gmail.com>
2021-09-13 04:00:32 +0100

    make package installable

---
+++ aiospotify/__init__.py
@@ -0,0 +1,33 @@
+# Future
+from __future__ import annotations
+
+# Standard Library
+import logging
+from typing import Final, Literal, NamedTuple
+
+# My stuff
+from .client import *
+from .exceptions import *
+from .objects import *
+from .utils import *
+from .values import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: Final[VersionInfo] = VersionInfo(major=2021, minor=9, micro=13, releaselevel="final", serial=0)
+
+__title__: Final[str] = "aiospotify"
+__author__: Final[str] = "Axelancerr"
+__copyright__: Final[str] = "Copyright 2021-present Axelancerr"
+__license__: Final[str] = "MIT"
+__version__: Final[str] = "2021.09.13"
+__maintainer__: Final[str] = "Aaron Hennessey"
+
+logging.getLogger("aiospotify")
---
+++ setup.py
@@ -0,0 +1,70 @@
+# Future
+from __future__ import annotations
+
+# Standard Library
+import os
+import re
+
+# Packages
+import setuptools
+
+
+with open("README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
+
+with open("requirements.txt") as file:
+    INSTALL_REQUIRES = file.read().splitlines()
+
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "aiospotify/__init__.py"))) as file:
+    VERSION = re.search(r"^__version__: [^=]* = \"([^\"]*)\"", file.read(), re.MULTILINE).group(1)
+
+
+CLASSIFIERS = [
+    "Framework :: AsyncIO",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Operating System :: OS Independent",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Typing :: Typed"
+]
+
+PROJECT_URLS = {
+    "Documentation": "https://aiospotify.readthedocs.io/en/latest/",
+    "Issue Tracker": "https://github.com/Axelancerr/aiospotify/issues",
+    "Source": "https://github.com/Axelancerr/aiospotify",
+}
+
+
+EXTRAS_REQUIRE = {
+    "docs": [
+        "sphinx",
+        "sphinxcontrib_trio",
+        "faculty-sphinx-theme",
+    ],
+}
+
+setuptools.setup(
+    author="Axelancerr",
+    classifiers=CLASSIFIERS,
+    description="An async Spotify API wrapper.",
+    extras_require=EXTRAS_REQUIRE,
+    install_requires=INSTALL_REQUIRES,
+    license="MIT",
+    name="Slate",
+    packages=["aiospotify", "typings"],
+    project_urls=PROJECT_URLS,
+    python_requires='>=3.9',
+    url="https://github.com/Axelancerr/aiospotify",
+    version=VERSION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+)
