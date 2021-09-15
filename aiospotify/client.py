c5a46cf7de26d8a096af3fb85888015e6fcc6e1c
Aaron Hennessey <axelancerr@gmail.com>
2021-09-16 00:06:46 +0100

    implement album methods

--- aiospotify/client.py
+++ aiospotify/client.py
@@ -1,16 +1,24 @@
 # Future
 from __future__ import annotations
 
+# Standard Library
+import math
+from collections import Sequence
+from typing import Any, TypeVar
+
 # Packages
 import aiohttp
 
 # My stuff
-from aiospotify import http, utils
+from aiospotify import http, objects, utils
 
 
 __all__ = (
     "Client",
 )
+
+
+ID = TypeVar("ID", bound=str)
 
 
 class Client:
@@ -31,9 +39,88 @@ class Client:
     def __repr__(self) -> str:
         return "<aiospotify.Client>"
 
+    #
+
     async def close(self) -> None:
 
         if not self.http._session:
             return
 
         await self.http.close()
+
+    # ALBUMS API
+
+    async def get_albums(
+        self,
+        ids: Sequence[ID],
+        *,
+        market: str | None = None
+    ) -> dict[ID, objects.Album | None]:
+
+        response = await self.http.get_albums(ids=ids, market=market)
+        return dict(zip(ids, [objects.Album(data) if data else None for data in response["albums"]]))
+
+    async def get_album(
+        self,
+        _id: str,
+        /,
+        *,
+        market: str | None = None
+    ) -> objects.Album:
+
+        response = await self.http.get_album(_id, market=market)
+        return objects.Album(response)
+
+    async def get_album_tracks(
+        self,
+        _id: str,
+        /,
+        *,
+        market: str | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> list[objects.SimpleTrack]:
+
+        response = await self.http.get_album_tracks(_id, market=market, limit=limit, offset=offset)
+        return [objects.SimpleTrack(data) for data in objects.PagingObject(response).items]
+
+    async def get_all_album_tracks(
+        self,
+        _id: str,
+        /,
+        *,
+        market: str | None = None,
+    ) -> list[objects.SimpleTrack]:
+
+        response = await self.http.get_album_tracks(_id, market=market, limit=50, offset=0)
+        paging = objects.PagingObject(response)
+
+        tracks = [objects.SimpleTrack(data) for data in paging.items]
+
+        if paging.total <= 50:  # There are 50 or less tracks and we already have them so just return them
+            return tracks
+
+        for _ in range(1, math.ceil(paging.total / 50)):
+            response = await self.http.get_album_tracks(_id, market=market, limit=50, offset=_ * 50)
+            tracks.extend([objects.SimpleTrack(data) for data in objects.PagingObject(response).items])
+
+        return tracks
+
+    async def get_full_album(
+        self,
+        _id: str,
+        /,
+        *,
+        market: str | None = None,
+    ) -> objects.Album:
+
+        album = await self.get_album(_id, market=market)
+
+        if album._tracks_paging.total <= 50:  # The album has 50 or less tracks already so we can just return it now.
+            return album
+
+        for _ in range(1, math.ceil(album._tracks_paging.total / 50)):
+            response = await self.http.get_album_tracks(_id, market=market, limit=50, offset=_ * 50)
+            album.tracks.extend([objects.SimpleTrack(data) for data in objects.PagingObject(response).items])
+
+        return album
